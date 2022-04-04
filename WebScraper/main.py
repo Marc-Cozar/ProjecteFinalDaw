64460cc32e3e46f777b54c692b7993b40a0f0b1e
Your Name <you@example.com>
2022-04-04 20:39:46 +0200

    half way db (got bored)

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -3,6 +3,22 @@ from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import os
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="laravel"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT user_login FROM streamers LIMIT 0, 5")
+
+mycursor2 = mydb.cursor()
+mycursor2.execute("SELECT user_login FROM streamers LIMIT 0, 5")
+
+products = mycursor.fetchall()
+web = mycursor2.fetchall()
+
 # Instantiate options
 opts = Options()
 # opts.add_argument(" — headless") # Uncomment if the headless version needed
@@ -14,21 +30,22 @@ chrome_driver = "chromedriver.exe"
 # Instantiate a webdriver
 driver = webdriver.Chrome(options=opts, executable_path=chrome_driver)
 
-# Load the HTML page
-driver.get("https://www.pccomponentes.com/buscar/?query=3090&")
+for x in products:
+    # Load the HTML page
+    driver.get("https://www.pccomponentes.com/buscar/?query=" + x.name)
 
-# Parse processed webpage with BeautifulSoup
-soup = BeautifulSoup(driver.page_source)
+    # Parse processed webpage with BeautifulSoup
+    soup = BeautifulSoup(driver.page_source)
 
-for span in soup.select('span:-soup-contains("€")'):    
-    try:
-        value = span.get_text()
-        parsedValue = value.replace('€', '').replace('.', '').replace(',', '.')
-        finalPrice = float(parsedValue)
-        print(finalPrice)
-        break
-    except:
-        print('JAJANT')
-        pass
+    for span in soup.select('span:-soup-contains("€")'):    
+        try:
+            value = span.get_text()
+            parsedValue = value.replace('€', '').replace('.', '').replace(',', '.')
+            finalPrice = float(parsedValue)
+            print(finalPrice)
+            break
+        except:
+            print('JAJANT')
+            pass
 
 driver.close()
