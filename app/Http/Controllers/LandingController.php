123684a00b6620dfeec271b2f8589c0041368e0f
Your Name <you@example.com>
2022-05-05 20:37:51 +0200

    Starting develop send mail & fix price generatos

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Web;
+use App\Mail\NotificationMail;
 use App\Models\ProductHistoric;
 use Exception;
 use Log;
@@ -29,6 +30,15 @@ class LandingController extends Controller
                 $product->web_prices()->detach($request->web_id);
                 $new_price = $product->web_prices()->attach($request->web_id, ['price' => $request->price]);
                 if($old_price && $old_price->pivot->price != $product->web_prices()->where('web_id', $request->web_id)->first()->pivot->price) {
+                    foreach(DB::table('user_email_notification')->where('web_id', $request->web_id)->where('product_id', $product->id) as $notification) {
+                        $user = User::find($notification->user_id);
+                        
+                        if($user) {
+                            $web = Web::find($request->web_id);
+                            Mail::to($user->email)->send(new NotificationMail(['old_price' => $old_price->pivot->price, 'new_price' => $request->price, 'web' => $web, 'user' => $user, 'product' => $product]));
+                        }
+                    }
+
                     ProductHistoric::updateOrCreate(['product_id' => $product->id, 'old_price' => $old_price->pivot->price, 'new_price' => $request->price, 'web_id' => $request->web_id]);
                 }   
             }
---
+++ app/Mail/NotificationMail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The notification object instance.
+     *
+     * @var Notification
+     */
+    public $notification;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($notification)
+    {
+        $this->notification = $notification;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('projectem12@gmail.com')
+                    ->view('mails.notification');
+    }
+}
